21c6d30ab9cb42e0fd2faffca16fffc185968cac
fxchAlice <mykhailovaleksandr3@gmail.com>
2023-09-19 00:11:43 +0300

    base of classes

--- src/Figure.java
+++ src/Figure.java
@@ -1,6 +1,13 @@
 public class Figure {
     private double leight;
+    private double width;
     public void setleight(double leight){
         this.leight = leight;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public double area(){
+        return leight * width;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Figure ABCD = new Figure();
+        ABCD.setWidth(3);
+        ABCD.setleight(1);
+        System.out.println(ABCD.area());
     }
 }
