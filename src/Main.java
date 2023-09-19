53e4e9205892492e4f9f8efc2319f492d313ff58
fxchAlice <mykhailovaleksandr3@gmail.com>
2023-09-19 20:23:04 +0300

    Structure changes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Figure ABCD = new Figure();
         ABCD.setWidth(3);
-        ABCD.setleight(1);
+        ABCD.setLeight(1);
         System.out.println(ABCD.diagonal());
         System.out.println(ABCD.round(ABCD.diagonal(), -2));
     }
