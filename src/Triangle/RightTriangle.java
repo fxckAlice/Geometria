0631f83a0aa9233f79f19f5112b2ba4b131a5e57
fxchAlice <mykhailovaleksandr3@gmail.com>
2023-09-24 22:28:24 +0300

    RightTriangle changes

--- src/Triangle/RightTriangle.java
+++ src/Triangle/RightTriangle.java
@@ -10,11 +10,9 @@ public class RightTriangle extends Triangle{
     public void setHeightB(double heightB) {
         System.out.println("Doesn't exist.");
     }
-    @Override
     public void setCornerC(double cornerC) {
         System.out.println("90 degrees is constant.");
     }
-    @Override
     public double area(char parameter){
         switch (parameter){
             case ('s'):
@@ -24,97 +22,5 @@ public class RightTriangle extends Triangle{
                 area = heightC * c / 2;
         }
         return area;
-    }
-    @Override
-    public double medianC(){
-        medianC = c / 2;
-        return medianC;
-    }
-    @Override
-    public double heightC(){
-        heightC = a * b / c;
-        return  heightC;
-    }
-    @Override
-    public double outRadius(){
-        outRadius = c / 2;
-        return outRadius;
-    }
-    @Override
-    public double inputRadius() {
-        inputRadius = (a + b - c) / 2;
-        return inputRadius;
-    }
-    @Override
-    public double side(char parameter) {
-        double answer;
-        switch (parameter) {
-            case 'o':
-                answer = Math.sqrt(Math.pow(outRadius * 2, 2) - Math.pow(b, 2));
-                break;
-            case 'i':
-                answer = inputRadius * 2 + c - b;
-                break;
-            case 'p':
-                answer = perimeter - b - c;
-                break;
-            case 'a':
-                answer = area / b * 2;
-                break;
-            case 't':
-                answer = Math.sqrt(Math.pow(c, 2) - Math.pow(b, 2));
-            default:
-                System.out.println("Enter correct parameter.");
-                answer = -1;
-        }
-        return answer;
-    }
-    @Override
-    public double sideB(char parameter) {
-        double answer;
-        switch (parameter) {
-            case 'o':
-                answer = Math.sqrt(Math.pow(outRadius * 2, 2) - Math.pow(a, 2));
-                break;
-            case 'i':
-                answer = inputRadius * 2 + c - a;
-                break;
-            case 'p':
-                answer = perimeter - a - c;
-                break;
-            case 'a':
-                answer = area / a * 2;
-                break;
-            case 't':
-                answer = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
-            default:
-                System.out.println("Enter correct parameter.");
-                answer = -1;
-        }
-        return answer;
-    }
-    @Override
-    public double sideC(char parameter) {
-        double answer;
-        switch (parameter) {
-            case 'o':
-                answer = outRadius * 2;
-                break;
-            case 'i':
-                answer = inputRadius * 2 - b - a;
-                break;
-            case 'p':
-                answer = perimeter - a - b;
-                break;
-            case 'm':
-                answer = medianC * 2;
-                break;
-            case 't':
-                answer = Math.sqrt(Math.pow(b, 2) + Math.pow(a, 2));
-            default:
-                System.out.println("Enter correct parameter.");
-                answer = -1;
-        }
-        return answer;
     }
 }
