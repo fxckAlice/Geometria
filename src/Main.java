7b447aa9460edc15f8ac4b230f9d2c404803ee27
fxchAlice <mykhailovaleksandr3@gmail.com>
2023-09-24 23:01:01 +0300

    round() added condition of using

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,5 @@ import Rectangle.Rhombus;
 public class Main {
     public static void main(String[] args) {
         Rectangle A = new Rectangle();
-        System.out.println(A.round(Math.cos((double) 60 / 180 * Math.PI) * 10, -8));
     }
 }
