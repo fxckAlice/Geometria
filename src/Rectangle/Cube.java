84610b84b93e8cd52a54790dacffb612c0f5b0f0
fxchAlice <mykhailovaleksandr3@gmail.com>
2023-09-28 01:06:36 +0300

    Cube

---
+++ src/Rectangle/Cube.java
@@ -0,0 +1,81 @@
+package Rectangle;
+
+public class Cube extends Square{
+    double volume, diagonalV;
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public void setDiagonalV(double diagonalV) {
+        this.diagonalV = diagonalV;
+    }
+    public double volume(){
+        volume = Math.pow(length, 3);
+        return volume;
+    }
+    public double diagonalV(){
+        diagonalV = length * Math.sqrt(3);
+        return diagonalV;
+    }
+    @Override
+    public double area(){
+        area = Math.pow(length, 2) * 6;
+        return area;
+    }
+    @Override
+    public double perimeter() {
+        System.out.println("Doesn`t exist");
+        return -1;
+    }
+    @Override
+    public double inputRadius() {
+        System.out.println("Doesn`t exist");
+        return -1;
+    }
+    @Override
+    public void setPerimeter(double perimeter) {
+        System.out.println("Doesn`t exist");
+    }
+    @Override
+    public void setInputRadius(double inputRadius) {
+        System.out.println("Doesn`t exist");
+    }
+    @Override
+    public void setOutRadius(double outRadius) {
+        System.out.println("Doesn`t exist");
+    }
+    @Override
+    public double outRadius (){
+        System.out.println("Doesn`t exist");
+        return -1;
+    }
+    @Override
+    public double sumOfCorners(){
+        System.out.println("Error!");
+        return -1;
+    }
+    @Override
+    public double side(char parameter){
+        double answer;
+        switch (parameter){
+            case ('a'):
+                answer = Math.sqrt(area / 6);
+                break;
+            case ('d'):
+                answer = Math.sqrt(Math.pow(diagonal, 2) / 2);
+                break;
+            case ('v'):
+                answer = Math.cbrt(volume);
+                break;
+            case('s'):
+                answer = diagonalV / Math.sqrt(3);
+                break;
+            default:
+                System.out.println("Enter correct parameter.");
+                answer = -1;
+                break;
+        }
+        return answer;
+    }
+}
