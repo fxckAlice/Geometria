558418e7aff3a28ec94f6e735f20f9ee5a8fee7f
fxchAlice <mykhailovaleksandr3@gmail.com>
2023-10-08 11:34:45 +0300

    StraightPrismIsoscelesTriangle

--- src/triangle/StraightPrismIsoscelesTriangle.java
+++ src/triangle/StraightPrismIsoscelesTriangle.java
@@ -1,10 +1,7 @@
 package triangle;
 
-import help.HelpingMethods;
-import org.w3c.dom.html.HTMLLegendElement;
-
-public class StraightPrismEquilateralTriangle extends EquilateralTriangle{
-    protected double heightP, volume, diagonalSide;
+public class StraightPrismIsoscelesTriangle extends IsoscelesTriangle{
+    protected double heightP, areaBase, areaSide, volume, diagonalSideA, diagonalSideB;
     @Override
     public void setPerimeter(double perimeter) {
         System.out.println("Doesn`t exist!");
@@ -18,10 +15,6 @@ public class StraightPrismEquilateralTriangle extends EquilateralTriangle{
         System.out.println("Doesn`t exist!");
     }
 
-    public void setDiagonalSide(double diagonalSide) {
-        this.diagonalSide = diagonalSide;
-    }
-
     public void setVolume(double volume) {
         this.volume = volume;
     }
@@ -29,18 +22,36 @@ public class StraightPrismEquilateralTriangle extends EquilateralTriangle{
     public void setHeightP(double heightP) {
         this.heightP = heightP;
     }
-    public double volume(){
-        volume = Math.pow(a, 2) * Math.sqrt(3) / 4 * heightP;
-        return volume;
-    }
-    public double diagonalSide(){
-        diagonalSide = help.pythagoreanTheorem(a, heightP, '+');
-        return diagonalSide;
+    public void setDiagonalSideA(double diagonalSideA) {
+        this.diagonalSideA = diagonalSideA;
     }
 
+    public void setDiagonalSideB(double diagonalSideB) {
+        this.diagonalSideB = diagonalSideB;
+    }
+    public double volume(){
+        volume = areaBase * heightP;
+        return volume;
+    }
+
+    public double areaBase(char parameter){
+        switch (parameter){
+            case ('a'):
+                areaBase = height * b / 2;
+                break;
+            case ('b'):
+                areaBase = heightB * a / 2;
+                break;
+        }
+        return areaBase;
+    }
+    public double areaSide()  {
+        areaSide = a * heightP * 2 + b * heightP;
+        return areaSide;
+    }
     @Override
-    public double area(){
-        area = Math.pow(a, 2) * Math.sqrt(3) / 2 + 3 * heightP * a;
+    public double area(char parameter){
+        area = areaBase * 2 + areaSide;
         return area;
     }
     @Override
@@ -58,14 +69,22 @@ public class StraightPrismEquilateralTriangle extends EquilateralTriangle{
         System.out.println("Doesn`t exist!");
         return -1;
     }
-    public double heightP(char parameter){
+    public double diagonalSideA(){
+        diagonalSideA = help.pythagoreanTheorem(a, heightP, '+');
+        return diagonalSideA;
+    }
+    public double diagonalSideB(){
+        diagonalSideB = help.pythagoreanTheorem(b, heightP, '+');
+        return diagonalSideB;
+    }
+    public double heightP(String parameter){
         double answer;
         switch (parameter){
-            case ('a'):
-                answer = (area / a - Math.sqrt(3) / 2) / 3;
+            case ("v"):
+                answer = volume / areaBase;
                 break;
-            case ('v'):
-                answer = volume / (Math.pow(a, 2) * Math.sqrt(3) / 4);
+            case ("aS"):
+                answer = areaSide / a;
                 break;
             default:
                 System.out.println("Enter correct parameter.");
